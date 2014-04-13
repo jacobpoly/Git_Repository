fd732d5887e1ca3d3baa5677066b5e1564a8f9d1
logan <logan@koreapolyschool.com>
2014-04-13 10:16:17 +0900

    Test_OP1

---
+++ Event_ Roulette/src/com/koreapolyschool/util/event/vo/ExcelVO.java
@@ -0,0 +1,125 @@
+package com.koreapolyschool.util.event.vo;
+
+public class ExcelVO {		// 엑셀 VO
+
+	private String member_code;	// 학번
+	private String name;				// 학생이름
+	private String edu_state;			// 교육상태
+	private String campus_name;	// 캠퍼스명
+	private String class_name;		// 클래스명
+	private String affiliate;			// 소속과정
+	private String product;				// 이벤트 상품
+	private String login_date;		// 로그인일시
+	private String participation;		// 이벤트 응모 일시
+	private String event_msg;		// 이벤트 메세지
+	private String enter_yn;		// 이벤트 응모여부
+	private String address;			// 주소
+	private String phone;				// 연락처
+
+	public String getMember_code() {
+		return member_code;
+	}
+	
+	public void setMember_code(String member_code) {
+		this.member_code = member_code;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getEdu_state() {
+		return edu_state;
+	}
+	
+	public void setEdu_state(String edu_state) {
+		this.edu_state = edu_state;
+	}
+	
+	public String getCampus_name() {
+		return campus_name;
+	}
+	
+	public void setCampus_name(String campus_name) {
+		this.campus_name = campus_name;
+	}
+	
+	public String getClass_name() {
+		return class_name;
+	}
+	
+	public void setClass_name(String class_name) {
+		this.class_name = class_name;
+	}
+	
+	public String getAffiliate() {
+		return affiliate;
+	}
+	
+	public void setAffiliate(String affiliate) {
+		this.affiliate = affiliate;
+	}
+	
+	public String getProduct() {
+		return product;
+	}
+	
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	
+	public String getLogin_date() {
+		return login_date;
+	}
+	
+	public void setLogin_date(String login_date) {
+		this.login_date = login_date;
+	}
+	
+	public String getParticipation() {
+		return participation;
+	}
+	
+	public void setParticipation(String participation) {
+		this.participation = participation;
+	}
+	
+	public String getEvent_msg() {
+		return event_msg;
+	}
+	
+	public void setEvent_msg(String event_msg) {
+		this.event_msg = event_msg;
+	}
+	
+	public String getEnter_yn() {
+		return enter_yn;
+	}
+	
+	public void setEnter_yn(String enter_yn) {
+		this.enter_yn = enter_yn;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	public String getPhone() {
+		return phone;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	
+	
+}
